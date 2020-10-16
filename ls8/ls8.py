ecf51e69d2ad656e76315311206704ea06754a33
Anthony N <foto.arn@gmail.com>
2020-10-16 10:41:12 -0700

    + In progress

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -4,8 +4,22 @@
 
 import sys
 from cpu import *
+from os import listdir
 
 cpu = CPU()
+file_list = {f:None for f in listdir("examples")}
 
-cpu.load()
+if len(sys.argv) > 1:
+    file_name = sys.argv[1]
+    if file_name in file_list:
+        cpu.load(file_name)
+    else:
+        print("invalid file name")
+        sys.exit(1)
+else:
+    cpu.load("sctest.ls8")
+        
+        
+# cpu.load()
 cpu.run()
+# print(file_list)
